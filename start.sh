9f938572b2bbb8b42600546fd6ecef637e2cf941
aiosdev <aiosdev@aiosdevs-Air.ht.home>
2023-07-11 19:32:51 -0400

    test workflow.

--- start.sh
+++ start.sh
@@ -1,1 +1,27 @@
-docker run -e MYSQL_ROOT_PASSWORD=password -e MYSQL_DATABASE=anpcrm -p 3306:3306 -d mysql:8.0
+ENVFILE=./app/.env.local
+if [ ! -f "$ENVFILE" ]
+then
+  mkdir ./app
+  tee -a ./app/.env.local <<EOF
+    DB_HOST=anypay-db-tmp.cbkm16y0krx6.ap-southeast-1.rds.amazonaws.com
+    DB_PORT=3306
+    DB_USER=root
+    DB_PASSWORD=XReBXQka9wwFYnUlz8Uv
+    DB_APIANYPAY=apianypay_db
+    DB_ANPCRM=anp_crm
+
+    AWS_REION=ap-southeast-1
+    AWS_S3_BUCKET=manage-documents.anypay.co.th
+    AWS_S3_PRESIGNED_EXPIRE_DURATION=3600
+
+    LINELIFF_ID='1584232670-QOz40bj9'
+EOF
+fi
+
+sudo docker pull 346774832960.dkr.ecr.ap-southeast-1.amazonaws.com/anp-crm:latest
+
+sudo docker run -v ./app/.env.local:/web/.env.local -p 80:3000 -d 346774832960.dkr.ecr.ap-southeast-1.amazonaws.com/anp-crm:latest
+
+aws ecr get-login-password --region ap-southeast-1 | sudo docker login --username AWS --password-stdin 346774832960.dkr.ecr.ap-southeast-1.amazonaws.com
+
+
